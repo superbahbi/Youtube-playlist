073d0bcebacabcc019e2cd73792e644bb22f3590
Bahbi <superbahbi@gmail.com>
2013-06-16 08:45:30 -0500

    ADDED ALL NEEDED FILES FROM NFS SERVER

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Neverpuddi playlist</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="../playlist/css/custom.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="../assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+	<?php 
+		$artist = $_GET['artist'];
+		$artistID = $_GET['id'];
+		$vnum = ($_GET['vid'])?$_GET['vid']:0;;
+	?>
+  <body>
+
+    <div class="container-narrow">
+
+      <div class="masthead row hlink">
+        <h3 ><a href="http://obscureserver.com">Neverpuddi Playlist</a></h3>
+		<h4 class="muted">Listen to your favorite artists, for free. </h4>
+      </div>
+
+      <hr>
+
+	<?php if ( !$artist && !$artistID): ?>
+		
+      <div class="jumbotron">
+        <h4>Who would you like to listen to?</h4>
+		<form>
+		  <fieldset>
+			<input name ="artist"type="text" placeholder="Type something"><br>
+			<button type="submit" class="btn-success">Submit</button>
+		  </fieldset>
+		</form>
+      </div>
+
+     <?php else: ?>
+	<?php 
+		include './playlist/data.php'; 
+		include './playlist/function.php';
+	?>
+	
+    <?php if ( $artistID &&  $videoData->response->total != 0): ?> 
+
+	 <div class="row-fluid marketing">
+
+<!-- TOP LEFT ----------------------------------------------------------------------------------------------------------------->
+        <div class="span6">
+          <h4><?php echo $artist_name?></h4>
+          <p>
+			<?php 		
+				foreach ($imagesData->response->images as $img){
+				echo '<img src='.$img->url.' width="200"><br>';
+			}?>
+		  </p>
+
+          <h4>Location</h4>
+          <p>
+		  <?php
+				$loc = $profileData->response->artist->artist_location;
+				if ($loc->city) echo "City : ".$loc->city.'<br>';
+				if ($loc->region) echo "State / Region : ".$loc->region.'<br>';
+				if ($loc->country) echo "Country : ".$loc->country.'<br>';
+		  ?>
+		  </p>
+
+          <h4>Links</h4>
+          <p>
+		 
+		  <?php
+			//todo hide if value is null
+		  	echo '<a href='.$urlData->response->urls->official_url.'>Official </a>';
+		  	echo '<a href='.$urlData->response->urls->lastfm_url.'> lastfm </a>';
+		  	echo '<a href='.$urlData->response->urls->myspace_url.'> myspace </a>';
+		  	echo '<a href='.$urlData->response->urls->itunes_url.'> iTunes </a>';
+		  ?>
+		  </p>
+        </div>
+<!-- TOP RIGHT (PLAYER)----------------------------------------------------------------------------------------------------------------->
+        <div class="span6">
+            <!--<h4>Playlist</h4>
+		    <h6 id="title"></h6>-->
+		    <div id="player"></div>
+		  	    <table class="table table-hover sidebar" >
+					<thead>
+						<tr>
+						    <th></th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php 
+						$next = 0;
+						$video_id_from_api = array();
+						$video_data = array();
+
+						foreach ( $videoData->response->video as $vid){
+							//TODO Filter bad data
+							$url = parse_url($vid->url);
+							parse_str($url['query'], $query);
+							array_push($video_id_from_api, $query['v']);				
+						}
+						//http://gdata.youtube.com/feeds/api/videos/cnMfwNMUf4U?v=2&alt=jsonc
+						$ytdata = new api();
+						foreach ($video_id_from_api as $video_id){
+							$ytdata->url = 'http://gdata.youtube.com/feeds/api/videos/'.$video_id.'?v=2&alt=jsonc';
+							try {
+								$youtube_data = $ytdata->get_api_data();
+							} catch (Exception  $e ) {
+								echo 'Caught exception: ',  $e->getMessage(), "\n";
+							}
+	
+
+							//check error and accesscontrol
+							if ( $youtube_data->error || !$youtube_data->accesscontrol->embed = 'allowed' ) {
+								continue;
+							}
+							//random data 
+							if ( $youtube_data->data->totalItems ){
+								continue;
+							}
+
+							//title filter
+							$title = strtolower($youtube_data->data->title);																// lower case the whole title string
+							$title = preg_replace('/[^A-Za-z0-9\. ]/', '', $title);																// delete all special characters
+			
+							$title = str_replace (strtolower($artist_name), "", $title);
+							$del_song_arr = array ('cover','reply','epk','live','interview','making');																// delete the song from the list if words match on the title
+							$remove_str_arr = array ('by','official','music','video','with','lyrics','description','hd');			// remove the word from the title if word/s match on the title
+							// Check if the video is good, no cover and other not okay content.
+							if ( check_str($del_song_arr, $title) ) {
+								continue;
+							}
+							// Remove some words we don't to be display in the title.
+							foreach ($remove_str_arr as &$ar) {
+								$ar = '/\b' . preg_quote($ar, '/') . '\b/';
+							}
+							$title = preg_replace($remove_str_arr, '', $title);
+							
+							//Final push for video data array hooraaa
+							array_push($video_data, array (
+								"id"=> $youtube_data->data->id,
+								"title"=> ucwords($title),
+								"viewcount" => $youtube_data->data->viewCount,
+							));
+						}
+						sort_array_of_array($video_data, 'viewcount');
+						foreach ( $video_data as $vid){
+							echo '<tr id='.$vid['id'].'>';
+								echo '<td href="javascript:void(0);" onclick="nextbybtn(\''.$vid['id'].'\','.$next++.');">'. $next .'. '. $vid['title'] .'</td>';
+							echo '</tr>';
+						}				
+					?>
+					</tbody>
+				</table>
+        </div>
+		
+<!-- CENTER BOTTOM ----------------------------------------------------------------------------------------------------------------->		
+		<div class="span12">
+			<h4>Similar artist</h4>
+			<p>
+				<?php 	
+					foreach( $similarData->response->artists as $sim){
+						echo '<a href=/?id='.$sim->id.'>'.$sim->name.'</a> - ';
+					}
+				?>
+			</p>
+        </div>
+<!-- ERROR PAGE ----------------------------------------------------------------------------------------------------------------->		
+			 <?php else: ?>
+			 <div class="row">
+				<p><?php echo "Sorry! We could not find any songs."; ?></p>
+			</div>
+		<?php endif; ?>
+     <?php endif; ?>
+      </div>
+
+
+      <div class="footer row" >
+        <p><a href="http://the.echonest.com" > <img src="./playlist/echo.png"> </a></p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+    <script>
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+	  var num = <?php echo $vnum; ?>;
+	  var len = 0;
+
+	  	<?php
+		$js_array = json_encode($video_data);
+		echo "var varIDjs = ". $js_array . ";\n";
+		?>
+		len = varIDjs.length - 1;
+		if ( len < num ) {
+			num = 0;
+		}
+		var id = varIDjs[num]['id'];
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '340',
+          width: '480',
+          videoId: id,
+		  playerVars: { 'autoplay': 1, 'controls': 1,  'showinfo': 0, },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+		//document.getElementById('title').innerHTML = varIDjs[num]['title'];
+		$("#" + varIDjs[num]['id']).addClass('highlight');
+		
+      }
+		
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      function onPlayerStateChange(event) {
+			if (event.data == YT.PlayerState.ENDED) {
+				$("#" + varIDjs[num]['id']).removeClass('highlight');
+				num = num + 1;
+				if ( num > len ) {
+					num = 0;
+				} 
+				next(num);
+			}
+      }
+	  	function next(n) {
+			player.loadVideoById(varIDjs[n]['id']);
+			player.playVideo();
+			//document.getElementById('title').innerHTML = varIDjs[num]['title'];
+			$("#" + varIDjs[num]['id']).addClass('highlight');
+		}
+      	function nextbybtn(s1, s2){
+			$("#" + varIDjs[num]['id']).removeClass('highlight');
+			num = s2 ;
+			//document.getElementById('title').innerHTML = varIDjs[num]['title'];
+			$("#" + varIDjs[num]['id']).addClass('highlight');
+			player.loadVideoById(s1,0);
+			player.playVideo();
+		}
+
+    </script>
+  </body>
+</html>
---
+++ playlist/data.php
@@ -0,0 +1,325 @@
+<?php 
+	include 'api.php';
+	include 'sort.php';
+	$apiKey = '7EYFZFIYDBJKNOL3R';
+	
+	$api = new api();
+	
+	//TODO : If search not found do suggest	
+	//http://developer.echonest.com/api/v4/artist/suggest?api_key=7EYFZFIYDBJKNOL3R&name=rad&results=5
+	//Stop loading if artist doesnt exist
+	
+	//search artist
+	//	{
+	//	  "response": {
+	//		"status": {
+	//		  "code": "0",
+	//		  "message": "Success",
+	//		  "version": "4.2"
+	//		},
+	//		"artists": {
+	//		  "artist": [
+	//			{
+	//			  "name": "Radiohead",
+	//			  "id": "ARH6W4X1187B99274F"
+	//			}
+	//		  ]
+	//		}
+	//	  }
+	//	}
+	if ( $artist ) {
+		$artist = preg_replace('/\s+/', '', $artist);
+		$api->url = 'http://developer.echonest.com/api/v4/artist/search?api_key='.$apiKey.'&format=json&name='.$artist.'&results=1';
+		try {
+		$search = $api->get_api_data();
+		} catch (Exception  $e ) {
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
+		} 
+
+		foreach ( $search->response->artists as $sec){
+			$artistID = $sec->id;
+		}
+	}
+
+	//biographies response
+	//	{
+	//	"response": {
+	//		"status": {
+	//			"version": "4.2",
+	//			"code": 0,
+	//			"message": "Success"
+	//		},
+	//		"start": 0,
+	//		"total": 12,
+	//		"biographies": [{
+	//			"text": "\"Radiohead\" are an English alternative rock band from Abingdon, Oxfordshire, formed in 1985. The band consists of Thom Yorke (vocals, guitars, piano), Jonny Greenwood (guitars, keyboards, other instruments), Ed O'Brien (guitars, backing vocals), Colin Greenwood (bass, synthesisers) and Phil Selway (drums, percussion).",
+	//			"site": "wikipedia",
+	//			"url": "http://en.wikipedia.org/wiki/Radiohead",
+	//			"license": {
+	//				"type": "cc-by-sa",
+	//				"attribution": "n/a",
+	//				"url": ""
+	//			}
+	//		}]
+	//	}
+	//}
+	//	foreach ( $data->response->biographies as $bio){
+	//	echo $bio->text;
+	//	echo $bio->text;
+	//}
+	
+	$api->url = 'http://developer.echonest.com/api/v4/artist/biographies?api_key='.$apiKey.'&id='.$artistID.'&format=json&results=1&start=0&license=cc-by-sa';
+	try {
+	$bioData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+
+	//images reponse
+	//	{
+	//	  "response": {
+	//		"status": {
+	//		  "code": "0",
+	//		  "message": "Success",
+	//		  "version": "4.2"
+	//		},
+	//		  "start": 0,
+	//		  "total": 121,
+	//		  "images": [
+	//			{
+	//			  "url": "http://userserve-ak.last.fm/serve/_/174456.jpg",
+	//			  "license": {
+	//				"url": "",
+	//				"attribution": "",
+	//				"type": "unknown"
+	//			  }
+	//			}
+	//		  ]
+	//		}
+	//	}
+	$api->url = 'http://developer.echonest.com/api/v4/artist/images?api_key='.$apiKey.'&id='.$artistID.'&format=json&results=1&start=0&license=unknown';
+	//result = number of picture
+	try {
+	$imagesData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	//foreach ($imagesData->response->images as $img){
+	//	echo '<img src='.$img->url.' ><br>';
+	//}
+	
+	//profile response
+	//	{
+	//		"response": {
+	//			"artist": {
+	//				"artist_location": {
+	//					"city": "Abingdon",
+	//					"country": "United Kingdom",
+	//					"location": "Abingdon, England, GB",
+	//					"region": "England"
+	//				},
+	//				"id": "ARH6W4X1187B99274F",
+	//				"name": "Radiohead"
+	//			},
+	//			"status": {
+	//				"code": 0,
+	//				"message": "Success",
+	//				"version": "4.2"
+	//			}
+	//		}
+	//	}
+	$api->url = 'http://developer.echonest.com/api/v4/artist/profile?api_key='.$apiKey.'&id='.$artistID.'&format=json&bucket=artist_location';
+	try {
+	$profileData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	$artist_name = $profileData->response->artist->name;
+	//$loc = $locationData->response->artist->artist_location;
+	//echo $loc->city;
+	//echo $loc->country;
+	//echo $loc->location;
+	//echo $loc->region;
+	
+	//similar response
+	//	{
+	//	  "response": {
+	//		"status": {
+	//		  "code": "0",
+	//		  "message": "Success",
+	//		  "version": "4.2"
+	//		},
+	//		  "artists": [
+	//			{
+	//			  "name": "Thom Yorke",
+	//			  "id": "ARH1N081187B9AC562"
+	//			}
+	//		  ]
+	//	  }
+	//	}
+	$api->url = 'http://developer.echonest.com/api/v4/artist/similar?api_key='.$apiKey.'&id='.$artistID.'&format=json&results=20&start=0';
+	try {
+	$similarData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	//foreach( $similarData->response->artists as $sim){
+	//	echo $sim->name.'<br>';
+	//}
+	
+	//twitter handle response
+	//	{
+	//		"response": {
+	//			"status": {
+	//				"version": "4.2",
+	//				"code": 0,
+	//				"message": "Success"
+	//			},
+	//			"artist": {
+	//				"id": "ARH6W4X1187B99274F",
+	//				"name": "Radiohead"
+	//				"twitter": "radiohead"
+	//			}
+	//		}
+	//	}
+	
+	$api->url = 'http://developer.echonest.com/api/v4/artist/twitter?api_key='.$apiKey.'&id='.$artistID.'&format=json';
+	try {
+	$twitter = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	//echo $twitter->response->artist->twitter;
+	
+	//url response
+	//	{
+	//		  "response": {
+	//			"status": {
+	//			  "code": "0",
+	//			  "message": "Success",
+	//			  "version": "4.2"
+	//			},
+	//			"urls": {
+	//				"lastfm_url": "http://www.last.fm/music/Radiohead",
+	//				"aolmusic_url": "http://music.aol.com/artist/radiohead",
+	//				"myspace_url": "http://www.myspace.com/radiohead",
+	//				"amazon_url": "http://www.amazon.com/gp/search?ie=UTF8&keywords=Radiohead&tag=httpechonecom-20&index=music",
+	//				"itunes_url": "http://itunes.com/Radiohead",
+	//				"mb_url": "http://musicbrainz.org/artist/a74b1b7f-71a5-4011-9441-d0b5e4122711.html"
+	//			}
+	//		  }
+	//	}
+	$api->url = 'http://developer.echonest.com/api/v4/artist/urls?api_key='.$apiKey.'&id='.$artistID.'&format=json';
+	try {
+	$urlData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	//echo 'Official : '.$urlData->response->urls->official_url.'<br>';
+	//echo 'Last.fm : '.$urlData->response->urls->lastfm_url.'<br>';
+	//echo 'Myspace : '.$urlData->response->urls->myspace_url.'<br>';
+	//echo 'Amazon : '.$urlData->response->urls->amazon_url.'<br>';
+	//echo 'iTunes : '.$urlData->response->urls->itunes_url.'<br>';
+	//echo 'Music Brainz : '.$urlData->response->urls->mb_url.'<br>';
+	
+	// video response
+	//	{
+	//	  "response": {
+	//		"status": {
+	//		  "code": "0",
+	//		  "message": "Success",
+	//		  "version": "4.2"
+	//		},
+	//		  "start": 0,
+	//		  "total": 121,
+	//		  "video": [
+	//			{
+	//			  "title": "Radiohead - Pyramid Song",
+	//			  "url": "http://youtube.com/watch?v=3M_Gg1xAHE4",
+	//			  "site": "youtube",
+	//			  "date_found": "2009-12-13T16:33:21",
+	//			  "image_url": "http://i4.ytimg.com/vi/3M_Gg1xAHE4/default.jpg",
+	//			  "id": "5da7ac8c7a1483af73eedf6f7f498307"
+	//			}
+	//		  ]
+	//		}
+	//	}
+
+
+	$api->url = 'http://developer.echonest.com/api/v4/artist/video?api_key='.$apiKey.'&id='.$artistID.'&format=json&results=30&start=0';
+	try {
+	$videoData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	
+
+		
+	//foreach ( $videoData->response->video as $vid){
+	//	echo $vid->url.'<br>';
+	//}
+	
+	
+	//news response
+	//	{
+	//	  "response": {
+	//		"status": {
+	//		  "code": "0",
+	//		  "message": "Success",
+	//		  "version": "4.2"
+	//		},
+	//		  "start": 0,
+	//		  "total": 121,
+	//		  "news": [
+	//			{
+	//			  "name": "First Listen: The National High Violet",
+	//			  "url": "http://www.drownedinsound.com/news/4139479-first-listen--the-national-high-violet",
+	//			  "date_posted": "2010-04-12T00:00:00",
+	//			  "date_found": "2010-04-12T06:15:29",
+	//			  "summary": " off a new-found sophistication (think of how <span>Radiohead</span>'s rhythm section developed over their first five records). Menacing cellos saw a triplet at a time, rising up like tall buildings around you, then rest. The contrast between Berninger's minimal, one-note vocals and the rising volume of the layers and layers of instruments builds tension until finally the singer shifts key, recognizing the urgency... and then the music dissolves into an atonal coda.  'Afraid of Everyone' Opening ... ",
+	//			  "id": "484a6108e649f0251c182381e401f0c6"
+	//			}
+	//		  ]
+	//		}
+	//	}	
+	$api->url = 'http://developer.echonest.com/api/v4/artist/news?api_key='.$apiKey.'&id='.$artistID.'&format=json&results=3&start=0';
+	try {
+	$newsData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	//foreach ( $newsData->response->news as $news) {
+	//	echo $news->url.'<br>';
+	//	echo $news->summary.'<br><br>';
+	//}
+	
+	//song response
+	//	{
+	//	  "response": {
+	//		"status": {
+	//		  "code": "0",
+	//		  "message": "Success",
+	//		  "version": "4.2"
+	//		},
+	//		  "start": 0,
+	//		  "total": 121,
+	//		  "songs": [
+	//			{
+	//			  "id": "SOXZYYG127F3E1B7A2",
+	//			  "title": "Karma police"
+	//			},
+	//			{
+	//			  "id": "SOXZABD127F3E1B7A2",
+	//			  "title" : "Creep"
+	//			}
+	//		  ]
+	//		}
+	//	  }
+	//	}
+	$api->url ='http://developer.echonest.com/api/v4/artist/songs?api_key=7EYFZFIYDBJKNOL3R&id=ARH6W4X1187B99274F&format=json&start=0&results=2';
+	try {
+	$songData = $api->get_api_data();
+	} catch (Exception  $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+?>
